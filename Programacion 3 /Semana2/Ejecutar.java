7c61e18af37b29617ef68d3792cb38d0d7f335e2
samuelcp1899 <sculma@estudiante.uniajc.edu.co>
2026-09-01 14:35:57 +0000

    llenar un arreglo de manera aleatoria

--- Programacion 3 /Semana2/Ejecutar.java
+++ Programacion 3 /Semana2/Ejecutar.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Ejecutar {
    public static void main(String[] args) {
      
@@ -7,6 +9,22 @@ public class Ejecutar {
       //recorremos el arreglo
       for (int i = 0; i < a.length; i++) {
          System.out.println("a[" + i + "]=" + a[i]);  
+      }
+      
+      //Forma 2 - llenando el arreglo de manera aleatoria
+      int[] b = new int[5]; //arerglo b de tamaño 5
+ 
+      int max = 100;
+      int min = 1;
+ 
+      //llenamos el arreglo b con números aleatorios entre 1 y 100
+      for (int i = 0; i < b.length; i++) {
+         b[i] = (int)(Math.random() * (max - min + 1)) + min;
+      }
+ 
+      //mostrar el arreglo
+      for (int i = 0; i < b.length; i++) {
+         System.out.println("b[" + i + "]=" + b[i]);  
       }
    }
 }
